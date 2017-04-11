19de673786e5307d95f285f92bb6d84e96390a13
Niko Myllynen <nmyllynen@gmail.com>
2017-04-11 16:35:55 +0300

    more info & vagrant user

--- customizations/scripts-common.sh
+++ customizations/scripts-common.sh
@@ -4,7 +4,7 @@
 read -r -p "Really pull production db? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-        vagrant ssh -- -t 'ruby /data/wordpress/customizations/insecure-wp-pull-production-db'
+      vagrant ssh -- -t 'ruby /data/wordpress/customizations/insecure-wp-pull-production-db && echo "Creating vagrant:vagrant admin" && wp user create vagrant vagrant@example.local --user_pass=vagrant --role=administrator && echo "Granting vagrant super-admin privileges (Error irrelevant if this is not a multisite install)" && wp super-admin add vagrant'
         ;;
     *)
         echo 'Production db not pulled.'
@@ -12,4 +12,5 @@ case "$response" in
 esac
 
 # Yarn
+echo 'Running yarn'
 yarn
