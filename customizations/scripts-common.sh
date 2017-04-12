33d405a1bfe5349b257c787b38f8f08b40189803
Niko Myllynen <nmyllynen@gmail.com>
2017-04-12 09:50:16 +0300

    add .env

--- customizations/scripts-common.sh
+++ customizations/scripts-common.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Pull production db for real
-read -r -p "Really pull production db? [y/N] " response
+# Actually pull database from production
+read -r -p "==> ksa: Actually pull database from production? (no): " response
 case "$response" in
-    [yY][eE][sS]|[yY])
-      vagrant ssh -- -t 'ruby /data/wordpress/customizations/insecure-wp-pull-production-db && echo "Creating vagrant:vagrant admin" && wp user create vagrant vagrant@example.local --user_pass=vagrant --role=administrator && echo "Granting vagrant super-admin privileges (Error irrelevant if this is not a multisite install)" && wp super-admin add vagrant'
-        ;;
-    *)
-        echo 'Production db not pulled.'
-        ;;
+  [yY][eE][sS]|[yY])
+    vagrant ssh -- -t 'ruby /data/wordpress/customizations/insecure-wp-pull-production-db && echo "==> ksa: Creating vagrant:vagrant admin" && wp user create vagrant vagrant@example.local --user_pass=vagrant --role=administrator && echo "==> ksa: Granting vagrant super-admin privileges (Error irrelevant if this is not a multisite install)" && wp super-admin add vagrant'
+    ;;
+  *)
+    echo '==> ksa: Production database not pulled.'
+    ;;
 esac
 
-# Composer
-composer self-update
+# Copy ENV if not copied yet
+if [ -f .env.example ] && [ ! -f .env ]; then
+  echo '==> ksa: Copying .env.example => .env'
+  cp .env.example .env
+fi
 
 # Yarn
-echo 'Running yarn'
+echo '==> ksa: Running yarn'
 yarn
+
+# Information
+echo '==> ksa: roots/bedrock documentation available at https://roots.io/bedrock/'
+echo '==> ksa: kehittamo-seravo-addons documentation available at https://bitbucket.org/kehittamo/kehittamo-seravo-addons'
